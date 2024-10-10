1b6df35df30acd72f77fdff3de83e0d84c48374c
Tushar Bansal <66954962+tusharb1308@users.noreply.github.com>
2024-10-10 12:52:24 +0530

    Flyweight Design Pattern

--- DesignPatterns/src/StructuralDesignPatterns/Flyweight/Main.java
+++ DesignPatterns/src/StructuralDesignPatterns/Flyweight/Main.java
@@ -1,4 +1,27 @@
 package StructuralDesignPatterns.Flyweight;
 
 public class Main {
+    private static final String[] colors = {"Red", "Green", "Blue", "Black", "White"};
+
+    private static int getRandomX(){
+        return (int) (Math.random()*100);
+    }
+
+    private static int getRandomY(){
+        return (int) (Math.random()*100);
+    }
+
+    private static String getRandomColor(){
+        return colors[(int)(Math.random()*colors.length)];
+    }
+
+    public static void main(String[] args) {
+        for(int i=0; i<20; i++){
+            Circle circle = (Circle) ShapeFactory.getCircle(getRandomColor());
+            circle.setX(getRandomX());
+            circle.setY(getRandomY());
+            circle.setRadius(100);
+            circle.draw();
+        }
+    }
 }
---
+++ DesignPatterns/src/StructuralDesignPatterns/Flyweight/Shape.java
@@ -0,0 +1,5 @@
+package StructuralDesignPatterns.Flyweight;
+
+public interface Shape {
+    void draw();
+}
